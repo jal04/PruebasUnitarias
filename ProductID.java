6a193c135a6fa78d85b34c44329e42d397e7b1c6
jal04 <josemi.bnf@outlook.es>
2020-01-11 17:06:14 +0100

    Creada clase ProductID

---
+++ ProductID.java
@@ -0,0 +1,29 @@
+package data;
+/**
+* The product identifying code in the National Health Service.
+*/
+final public class ProductID {
+    private final String productID;
+
+    public ProductID(String code) { 
+        this. productID = code; 
+    }
+    public String getProductID() {
+        return productID;
+        }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductID universalpID = (ProductID) o;
+        return productID.equals(universalpID.productID);
+    }
+    @Override
+    public int hashCode() {
+        return productID.hashCode();
+    }
+    @Override
+    public String toString() {
+        return "UniversalProductID{" + "product code='" + productID + '\'' + '}';
+    }
+}
