876dfe116876c31e66e739568fed5c420c607032
jal04 <josemi.bnf@outlook.es>
2020-01-11 18:24:30 +0100

    Creado test para PatientContr

--- PatientContr.java
+++ PatientContr.java
@@ -6,18 +6,12 @@ final public class PatientContr {
     private final BigDecimal amount_paid;  //porcentaje que paga el paciente
 
     public ProductID(String cantidad) {
+        if(cantidad==null) throw NullPointerException("amount is null");
         this.amount_paid = cantidad; 
     }
     public String getAmountPaid() {
         return amount_paid;
         }
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ProductID percentAmountPaid = (PatientContr) o;
-        return amount_paid.equals(percentAmountPaid.amount_paid);
-    }
     @Override
     public int hashCode() {
         return amount_paid.hashCode();
