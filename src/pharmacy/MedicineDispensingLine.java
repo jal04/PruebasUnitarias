ad261c778b3891659b6d4d88a27d871644f6121c
jal04 <josemi.bnf@outlook.es>
2020-01-12 17:50:38 +0100

    Eliminamos el atributo productID de la clase MedicineDispensingLine ya que este es el mismo que el atributo UPCcode de ProductSpecification

--- src/pharmacy/MedicineDispensingLine.java
+++ src/pharmacy/MedicineDispensingLine.java
@@ -3,15 +3,12 @@ package pharmacy;
 import data.ProductID;
 
 public class MedicineDispensingLine {
-    ProductID productID;
     ProductSpecification productSpecification;
     Boolean acquired;
-    public MedicineDispensingLine(ProductID productID, ProductSpecification productSpecification){
-        this.productID=productID;
+    public MedicineDispensingLine(ProductSpecification productSpecification){
         this.productSpecification=productSpecification;
         this.acquired=false;
     }
-
     public void productAcquired(){
         this.acquired=true;
     }
